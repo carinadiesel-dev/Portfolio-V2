ca1a5271bd9fe9f31b2e6ca176fd0375fa7785df
Carina Diesel <106066035+carinadiesel-dev@users.noreply.github.com>
2022-11-05 16:03:16 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors: {
+      'Dark Jungle Green': '111c1a',
+      'Atomic Tangerine': 'ff9d70',
+      'Maximum Blue Purple': '9396f0',
+      'Metallic Seaweed': '18808c',
+      'International Klein Blue': '022fb6',
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
